713accb4e750406526c84f6b779e9fb02a3495a2
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-10-26 12:48:18 -0400

    Update

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -2,4 +2,35 @@
 
 public class Account {
 
+    private float balance;
+    private String type;
+
+    public Account(float initialDeposit, String type){
+        this.balance = initialDeposit;
+        this.type = type;
+    }
+
+    public void deposit(float amount){
+
+    }
+
+
+
+    
+    //getters and setters
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
