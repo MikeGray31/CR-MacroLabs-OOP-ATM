e5d841678a52cb7da40b1b6a238068078bc9ad2d
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2019-10-26 16:58:17 -0400

    Created User constructor

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -2,4 +2,34 @@
 
 public class User {
 
+    private String name;
+    private String iD;
+    private String password;
+    private float balance;
+
+
+    public User(String name, String iD, String password, float balance){
+        this.name = name;
+        this.iD = iD;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    //getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getiD() {
+        return iD;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
 }
---
+++ src/main/tests/SavingsTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class SavingsTest {
+
+    @Test
+    public void interestDollarTest(){
+
+    }
+}
